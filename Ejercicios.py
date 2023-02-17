729c7e4359f966414bc0b64c437a02bbc1ba365c
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-02-16 22:32:54 -0500

    Ejercicio 1

---
+++ Ejercicios.py
@@ -0,0 +1,17 @@
+"""--------- Ejercicio 1 -----------"""
+#1. Qué operadores utiliza Python en los siguientes casos:
+
+#A. División Modular                %
+#B. Exponenciación                  **
+#C. División que retorne entero.    //
+
+print("División modular % \nExponenciación ** \nDivisión que retorne entero //")
+
+numero1 = int(input(f" \t 1. -> Digite un numero: "))
+numero2 = int(input(f" \t 2. -> Digite un numero: "))
+
+print(f"La división modular de {numero1} % {numero2} = {numero1 % numero2}") 
+print(f"La exponenciación de {numero1} ** {numero2} = {numero1 ** numero2}") 
+print(f"La división de {numero1} // {numero2} = {numero1 // numero2}") 
+
+""" ------------------------------- """
