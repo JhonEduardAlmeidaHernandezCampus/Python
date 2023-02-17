93faa547612a56dee09d5544f87b91fe6d692619
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-02-16 22:37:45 -0500

    Ejercicio 5

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,7 +1,22 @@
-"""--------- Ejercicio 4 -----------"""
+"""--------- Ejercicio 5 -----------"""
 
-#Que son las expresiones regulares en Python?
+#Enumere 5 tipos de datos en Python y suministre un valor de ejemplo de cada uno.
+#int         5
+#float       3.1416
+#bool        True and False
+#str         Jhon Almeida
+#list        [1, "Jhon", 3, 19, "Bicicleta"]
 
-print("Las expresiones son combinaciones de constantes, variables, símbolos y paréntesis, que arrojan un valor tal y como se conocen en la notación traducuinal de las matemáticas")
+int = 5
+float = 3.1416
+bool = False
+str = "Jhon Almeida"
+list = [1, "Jhon", 3, 19, "Bicicleta"]
+
+print(f"Esto es un dato tipo Entero: {int}")
+print(f"Esto es un dato tipo Float: {float}")
+print(f"Esto es un dato tipo Booleano: {bool}")
+print(f"Esto es un dato tipo Cadena: {str}")
+print(f"Esto es un dato tipo Secuencia o Lista: {list}") 
 
 """ ------------------------------- """
