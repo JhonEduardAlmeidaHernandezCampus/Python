d93028ded85ceaf356f6d2fecb39cd9f1498063f
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-02-16 22:43:51 -0500

    Ejercicio 8

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,8 +1,15 @@
-"""--------- Ejercicio 7 -----------"""
+"""--------- Ejercicio 8 -----------"""
 
-#Cuál es la diferencia entre un condicional simple y un condicional compuesto?
+#Escriba un bloque cualquiera de código en Python en donde utilice 2 condicionales (if) anidados.
 
-print("El condicional simple, es el que ejecuta una sola instrucción para llegar al resultado")
-print("El condicional compuesto, es el que ejecuta varias instrucciones para llegar al resultado")
+edad = int(input("Escribe tu edad: "))
+grupo = input("Escribe a que grupo quieres pertenecer [A o B]: ")
+
+if edad >= 18 and grupo == "A" or grupo == "B":
+	print(f"Puedes entrar al grupo {grupo}")
+elif edad < 18 and grupo == "A" or grupo == "B":
+	print(f"No puedes entrar al grupo {grupo} si eres menor de edad")
+else:
+	print("Elección no válida")
 
 """ ------------------------------- """
