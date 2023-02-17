53cfcb3d84b5f025728f25f54551dfb300741be5
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-02-16 22:41:57 -0500

    Ejercicio 7

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,13 +1,8 @@
-"""--------- Ejercicio 6 -----------"""
+"""--------- Ejercicio 7 -----------"""
 
-#En sus propias palabras, qué son las funciones preconstruidas y proporcione 2 ejemplos.
+#Cuál es la diferencia entre un condicional simple y un condicional compuesto?
 
-lista = [1, 54, 'Jhon']
-lista2 = [2, 14, 1, 9, 0, 3]
-resultado = min(lista2)
-
-print("Son funciones que ya vienen integradas en Python, como por ejemplo:")
-print("-> List = Las listas \n-> Print = Imprimir por pantalla el argumento que le pasemos \n-> Min = Determina el numero menor en una lista de numeros")
-print(f"-> Ejemplos: \n Listas: {lista} \n Min = {resultado} lista de ejemplo: {lista2}") 
+print("El condicional simple, es el que ejecuta una sola instrucción para llegar al resultado")
+print("El condicional compuesto, es el que ejecuta varias instrucciones para llegar al resultado")
 
 """ ------------------------------- """
